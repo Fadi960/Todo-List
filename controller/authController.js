d4d8d1b481a0d81634967fc4fb639dba31169fbe
Fadi960 <fa1712320@gmail.com>
2024-05-17 15:55:39 +0500

    task

--- controller/authController.js
+++ controller/authController.js
@@ -5,6 +5,18 @@ const loginSchema = joi.object().keys({
   password: joi.string().min(6).max(18).required(),
 })
 
+const updateUserSchema = joi.object().keys({
+  username: joi.string().email(),
+  password: joi.string().min(6).max(18),
+});
+
+const createUserSchema = joi.object().keys({
+  username: joi.string().email().required(),
+  password: joi.string().min(6).max(18).required(),
+});
+
+const deleteUserSchema = joi.object()
+
 module.exports = {
     login: async (req, res) => {
       try{
@@ -20,7 +32,52 @@ module.exports = {
           });
         }
 },
-  
+
+   updateUser: async (req, res) => {
+  try{
+  const validate = await updateUserSchema.validateAsync(req.body);
+  console.log(req.body);
+    return res.send({
+    message: "update user Api",
+    data: validate,
+    });
+  }catch(error){
+      return res.status(400).send({
+        message: error.message,
+      });
+    }
+},
+
+createUser: async (req, res) => {
+  try {
+    const validate = await createUserSchema.validateAsync(req.body);
+    console.log(req.body);
+    return res.send({
+      message: "create user Api",
+      data: validate,
+    });
+  } catch (error) {
+    return res.status(400).send({
+      message: error.message,
+    });
+  }
+},
+
+deleteUser: async (req, res) => {
+  try {
+    const validate = await deleteUserSchema.validateAsync(req.body);
+    console.log(req.body);
+    return res.send({
+      message: "Delete user Api",
+      data: validate,
+    });
+  } catch (error) {
+    return res.status(400).send({
+      message: error.message,
+    });
+  }
+},
+
   logout: (req, res) => {
     console.log(req.body);
     return res.send({
@@ -30,8 +87,10 @@ module.exports = {
   },
 
   resetPassword: (req,res) => {
+    console.log(req.query);
     return res.send({
         message: "reset password Api",
+        data: req.query
     });
-},
-  };
+  }
+}
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,9 +4,15 @@ const {
     login,
     logout,
     resetPassword,
+    updateUser,
+    deleteUser,
+    createUser,
 } = require("../controller/authController");
 
 route.get("/login", login);
+route.put("/updateUser", updateUser);
+route.post("/createUser", createUser);
+route.delete("/deleteUser", deleteUser)
 route.get("/logout",logout);
 route.get("/resetPassword",resetPassword);
 
