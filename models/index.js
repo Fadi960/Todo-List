406139ce29f626d62f7b07790945be6f5c2779d4
Fadi960 <fa1712320@gmail.com>
2024-05-21 21:01:15 +0500

    dbconnection

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const sequelize = require("../bin/dbConnection");
+
+const users = require("./definitions/users");
+const tasks = require("./definitions/tasks")
+
+const models = {users, tasks};
+
+const db = {};
+
+db.sequelize = sequelize;
+sequelize.models = models;
+
+module.exports = {db, models};
